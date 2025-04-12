9e9df4e653d46c1c95b65b3182710b539d26e9a3
Vladimir80Utkin <107009358+Vladimir80Utkin@users.noreply.github.com>
2025-04-12 14:41:37 +0300

    Добавлен класс Flat

---
+++ Flat.cs
@@ -0,0 +1,47 @@
+using System.Text.RegularExpressions;
+
+namespace cours_project
+{
+    class Flat
+    {
+        private string  tenantFullName;
+        private string flatAddress;
+        private int peopleCount;
+        private double tariffPerPerson;
+
+        public string TenantFullName{
+            get{return tenantFullName;}
+            set{
+                if (string.IsNullOrWhiteSpace(value)) throw new ArgumentException("Имя арендатора не может быть пустым!");
+                if (!Regex.IsMatch(value, @"^[A-ZА-Я][a-zа-я]*(\s[A-ZА-Я][a-zа-я]*)*$")) throw new ArgumentException("Имя арендатора должно начинаться с заглавной буквы, содержать только буквы и один пробел между словами!");
+                else tenantFullName = value;
+            }
+        }
+        public string FlatAddress{
+            get{return flatAddress;}
+            set{
+                if (string.IsNullOrWhiteSpace(value)) throw new ArgumentException("Адрес квартиры не может быть пустым!");
+                else flatAddress = value;
+            }
+        }
+        public int PeopleCount{
+            get{return peopleCount;}
+            set{
+                if (value < 1) throw new ArgumentException("Количество человек не может быть меньше 1!");
+                else peopleCount = value;
+            }
+        }
+        public double TariffPerPerson{
+            get{return tariffPerPerson;}
+            set{
+                if (value < 1) throw new ArgumentException("Тариф не может быть иеньше 1!");
+                else tariffPerPerson = value;
+            }
+        }
+
+        public Flat(string _tenantFullName, string _flatAddress){
+            tenantFullName = _tenantFullName;
+            flatAddress = _flatAddress;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,27 +1,33 @@
-﻿
-ConsoleKeyInfo key;
-do
+﻿using Newtonsoft.Json;
+namespace cours_project
 {
-    ShowMenu();
-    key = Console.ReadKey(true);
-    switch (key.Key)
-    {
-        
-    }
-    
-} 
-while (key.Key != ConsoleKey.Escape);
+    class Program{
+        static void Main(){
+            try
+            {
+                Flat flat1 = new Flat("Ivan Ivanov", "1234 Elm Street");
+                Console.WriteLine("фио: " + flat1.TenantFullName);
+                Console.ReadKey();      
+            }
+            catch (ArgumentException exception)
+            {
+                Console.WriteLine("Ошибка: " + exception.Message);
+                Console.ReadKey();
+            }
+        }
 
-static void ShowMenu(){
-    Console.Clear();
-    Console.WriteLine("F1 - Ввод данных");
-    Console.WriteLine("F2 - Запись данных в файл");
-    Console.WriteLine("F3 - Чтение данных из файла");
-    Console.WriteLine("F4 - Вывод данных на экран");
-    Console.WriteLine("F5 - Дополнение данных");
-    Console.WriteLine("F6 - Удаление данных");
-    Console.WriteLine("F7 - Поиск информации");
-    Console.WriteLine("F8 - Сортировка данных (вставками)");
-    Console.WriteLine("F9 - Построение диаграммы");
-    Console.WriteLine("ESC - Выход");
+        static void ShowMenu(){
+            Console.Clear();
+            Console.WriteLine("F1 - Ввод данных");
+            Console.WriteLine("F2 - Запись данных в файл");
+            Console.WriteLine("F3 - Чтение данных из файла");
+            Console.WriteLine("F4 - Вывод данных на экран");
+            Console.WriteLine("F5 - Дополнение данных");
+            Console.WriteLine("F6 - Удаление данных");
+            Console.WriteLine("F7 - Поиск информации");
+            Console.WriteLine("F8 - Сортировка данных (вставками)");
+            Console.WriteLine("F9 - Построение диаграммы");
+            Console.WriteLine("ESC - Выход");
+        }
+    }
 }
